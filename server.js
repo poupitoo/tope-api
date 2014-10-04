86011aede6b07aaf3c652d702dcd944f78f76ab3
Samix <reaverhack@gmail.com>
2014-10-04 12:42:14 +0200

    Prod version

---
+++ server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// Module dependencies.
+var express = require('express');
+
+var app = express();
+
+
+// Express Configuration
+require('./lib/config/express')(app);
+
+// Controllers
+var api = require('./lib/controllers/api'),
+		user = require('./lib/controllers/models/user'),
+		top = require('./lib/controllers/models/top'),
+    index = require('./lib/controllers');
+
+// Server Routes
+app.get('/api/awesomeThings', api.awesomeThings);
+
+
+//Users
+app.get('/api/users', user.getUsers);
+app.post('/api/user', user.createUser);
+app.put('/api/user/:id/name/:name/email/:email',user.updateInfo);
+
+
+//Top
+app.get('/api/tap/:userId/timestamp/:timestamp', top.checkTop);
+
+//app.post('/api/user',api.)
+
+
+// app.get('/partials/*', index.partials);
+// app.get('/*', index.index);
+
+// Start server
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Express server listening on port %d in %s mode', port, app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;
---
+++ test-webservice.sh
@@ -0,0 +1,15 @@
+#Get Users
+curl http://localhost:3000/api/users
+
+curl http://10.12.20.174/api/users
+
+#Create User
+curl --data "param1=value1&param2=value2" http://localhost:3000/api/user
+
+#Update User Info
+curl -X PUT --data "id=0&name=value2&email=value3" http://localhost:3000/api/user/0/name/samix/email/samix@samix.co
+
+#Check top
+curl http://localhost:3000/api/tap/0/timestamp/100/
+
+curl http://localhost:3000/api/tap/1/timestamp/100/
