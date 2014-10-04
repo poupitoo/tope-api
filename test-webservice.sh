fff8ae3174b0cd770a7effda8ac6c04abb8f885e
Samix <reaverhack@gmail.com>
2014-10-04 18:16:00 +0200

    update WS

--- test-webservice.sh
+++ test-webservice.sh
@@ -7,7 +7,7 @@ curl http://10.12.20.174/api/users
 curl --data "param1=value1&param2=value2" http://localhost:3000/api/user
 
 #Update User Info
-curl -X PUT --data "id=0&name=value2&email=value3" http://localhost:3000/api/user/0/name/samix/email/samix@samix.co
+curl -X PUT --data "id=0&name=value2&username=value3&phone=23232323&email=value3" http://localhost:3000/api/user/0/name/samix/email/samix@samix.co
 
 #Check top
 curl http://localhost:3000/api/tap/0/timestamp/100/
